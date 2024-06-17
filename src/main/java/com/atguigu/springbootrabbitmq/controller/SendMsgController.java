c607963c63e8ac82c190bb60804e4bb71adf0746
lihongfei <2529574569@qq.com>
2024-06-17 23:19:50 +0800

    延时队列优化

---
+++ src/main/java/com/atguigu/springbootrabbitmq/controller/SendMsgController.java
@@ -0,0 +1,53 @@
+package com.atguigu.springbootrabbitmq.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * 发送延迟消息
+ *
+ *  http://localhost:8080/ttl/sendMsg/嘻嘻嘻
+ */
+@Slf4j
+@RestController
+@RequestMapping("/ttl")
+public class SendMsgController {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    // 开始发消息
+    @GetMapping("/sendMsg/{message}")
+    public void sendMsg(@PathVariable("message") String message) {
+
+        log.info("当前时间:{},发送一条消息给两个TTL队列:{}", new Date().toString(), message);
+
+        // 发送消息
+        rabbitTemplate.convertAndSend("X", "XA", "消息来自 ttl 为 10S 的队列: " + message);
+        rabbitTemplate.convertAndSend("X", "XB", "消息来自 ttl 为 40S 的队列: " + message);
+    }
+
+    // 开始发消息 消息 TTL
+    @GetMapping("/sendExpirationMsg/{message}/{ttlTime}")
+    public void sendMsg(@PathVariable("message") String message,
+                        @PathVariable("ttlTime") String ttlTime) {
+
+        log.info("当前时间:{},发送一条时长为{}毫秒 TTL消息给队列QC:{}",
+                new Date().toString(), ttlTime, message);
+
+        // 设置消息的 TTL
+        rabbitTemplate.convertAndSend("X","XC",message,msg -> {
+            // 发送消息的时候 延迟时长
+            msg.getMessageProperties().setExpiration(ttlTime);
+            return msg;
+        });
+
+    }
+}
